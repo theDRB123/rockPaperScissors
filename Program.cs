0f4180c19ce28fefcb1483c6a639a966ff1605e5
theDRB123 <dhrbhanushali123@gmail.com>
2023-04-26 12:43:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockPaperSiccors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //? ttt
+            Random random = new Random();
+            bool playAgain = true;
+            string player = "";
+            string computer;
+            while (playAgain)
+            {
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine("Enter Rock , Paper or Scissors");
+                    player = Console.ReadLine();
+                    //converting to upper for consistency
+                    player = player.ToUpper();
+                }
+                
+
+
+
+            }
+        }
+    }
+}
